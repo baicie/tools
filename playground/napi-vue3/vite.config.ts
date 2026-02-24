57d75ef5b769d2c00f9ee7c383a88d40d37f1654
baicie <2055572879@qq.com>
2026-02-24 17:14:36 +0800

    chore: set base path for Vite configuration in multiple playground projects

--- playground/napi-vue3/vite.config.ts
+++ playground/napi-vue3/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/napi-vue3/',
   plugins: [vue()],
   optimizeDeps: {
     exclude: ['@baicie/napi-browser'],
--- playground/scripts/vite.config.ts
+++ playground/scripts/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/storage/',
+  base: '/scripts/',
 })
